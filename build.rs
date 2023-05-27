4868d6d65f48671fe39a05754c189a191aa3046a
Victor Hornet <victorhornet@pm.me>
2023-05-27 16:57:59 +0200

    Move language files to src/language

--- build.rs
+++ build.rs
@@ -5,10 +5,10 @@ fn main() {
     CTLexerBuilder::new()
         .lrpar_config(|ctp| {
             ctp.yacckind(YaccKind::Grmtools)
-                .grammar_in_src_dir("typeling.y")
+                .grammar_in_src_dir("language/typeling.y")
                 .unwrap()
         })
-        .lexer_in_src_dir("typeling.l")
+        .lexer_in_src_dir("language/typeling.l")
         .expect("Initializing LexerBuilder failed")
         .build()
         .expect("Lexer build failed");
